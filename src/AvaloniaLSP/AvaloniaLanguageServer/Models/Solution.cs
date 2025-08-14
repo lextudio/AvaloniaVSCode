4df53434dab37bc383d341ce30941f0518d3fc0b
Lex Li <support@lextm.com>
2025-08-13 23:23:15 -0400

    Upgraded to AvaloniaVS 11.8. Addressed compilation warnings

--- src/AvaloniaLSP/AvaloniaLanguageServer/Models/Solution.cs
+++ src/AvaloniaLSP/AvaloniaLanguageServer/Models/Solution.cs
@@ -1,16 +1,17 @@
 using System.Text.Json.Serialization;
+using System;
 namespace AvaloniaLanguageServer.Models
 {
     public partial class SolutionData
     {
         [JsonPropertyName("solution")]
-        public string Solution { get; set; }
+        public string Solution { get; set; } = string.Empty;
 
         [JsonPropertyName("projects")]
-        public Project[] Projects { get; set; }
+        public Project[] Projects { get; set; } = Array.Empty<Project>();
 
         [JsonPropertyName("files")]
-        public ProjectFile[] Files { get; set; }
+        public ProjectFile[] Files { get; set; } = Array.Empty<ProjectFile>();
 
         public Project? GetExecutableProject()
         {
@@ -21,50 +22,50 @@ namespace AvaloniaLanguageServer.Models
     public partial class ProjectFile
     {
         [JsonPropertyName("path")]
-        public string Path { get; set; }
+        public string Path { get; set; } = string.Empty;
 
         [JsonPropertyName("targetPath")]
-        public string TargetPath { get; set; }
+        public string TargetPath { get; set; } = string.Empty;
 
         [JsonPropertyName("projectPath")]
-        public string ProjectPath { get; set; }
+        public string ProjectPath { get; set; } = string.Empty;
     }
 
     public partial class Project
     {
         [JsonPropertyName("name")]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         [JsonPropertyName("path")]
-        public string Path { get; set; }
+        public string Path { get; set; } = string.Empty;
 
         [JsonPropertyName("targetPath")]
-        public string TargetPath { get; set; }
+        public string TargetPath { get; set; } = string.Empty;
 
         [JsonPropertyName("outputType")]
-        public string OutputType { get; set; }
+        public string OutputType { get; set; } = string.Empty;
 
         [JsonPropertyName("designerHostPath")]
-        public string DesignerHostPath { get; set; }
+        public string DesignerHostPath { get; set; } = string.Empty;
 
         [JsonPropertyName("targetFramework")]
-        public string TargetFramework { get; set; }
+        public string TargetFramework { get; set; } = string.Empty;
 
         [JsonPropertyName("depsFilePath")]
-        public string DepsFilePath { get; set; }
+        public string DepsFilePath { get; set; } = string.Empty;
 
         [JsonPropertyName("runtimeConfigFilePath")]
-        public string RuntimeConfigFilePath { get; set; }
+        public string RuntimeConfigFilePath { get; set; } = string.Empty;
 
         [JsonPropertyName("projectReferences")]
-        public string[] ProjectReferences { get; set; }
+        public string[] ProjectReferences { get; set; } = Array.Empty<string>();
 
         [JsonPropertyName("directoryPath")]
-        public string DirectoryPath { get; set; }
+        public string DirectoryPath { get; set; } = string.Empty;
 
 
         [JsonPropertyName("intermediateOutputPath")]
-        public string IntermediateOutputPath { get; set; }
+        public string IntermediateOutputPath { get; set; } = string.Empty;
     }
 
 }
